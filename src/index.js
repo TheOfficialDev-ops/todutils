76c45cf89a4f3c6cf8cd69f0ee93b9bd1040f70b
The Official Dev <theofficial.dev204@gmail.com>
2021-09-11 15:47:08 -0500

    v1.0.1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 // Copyright (c) The Official Dev
 // TOD License v1
 
+const version = '1.0.1'
+
 const loader = require('./loader')
 const Log = require('./u/logger')
 const { join } = require('path')
 
 module.exports = (dirname, folder, ...args) => {
-    if(!dirname) return new Log('No dirname specified.', 'todutils')
-    if(!folder) return new Log('No utils folder specified.', 'todutils')
+    if(!dirname) return new Log('No dirname specified.', 'todutils', true)
+    if(!folder) return new Log('No utils folder specified.', 'todutils', true)
+
+    new Log(`Loading The Official Dev Utils v.${version}`)
     
     return loader(join(dirname, folder), ...args)
 }
